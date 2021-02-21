904d631faa037d86d1df92d7e62bfe4d03b2d535
Rodrigo Lessinger <rodrigo.lessinger@nubank.com.br>
2021-02-21 12:18:48 -0300

    Create basic RecipeCard

---
+++ app/src/main/java/com/rodrigolessinger/thefoodapp/presentation/list/ui/RecipeCard.kt
@@ -0,0 +1,32 @@
+package com.rodrigolessinger.thefoodapp.presentation.list.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rodrigolessinger.thefoodapp.data.model.Recipe
+import dev.chrisbanes.accompanist.coil.CoilImage
+
+@Composable
+fun RecipeCard(recipe: Recipe) {
+    Card {
+        Column {
+            CoilImage(data = recipe.thumbnail, contentDescription = "")
+            Text(text = recipe.description)
+            Text(text = recipe.name)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RecipeCardPreview() {
+    RecipeCard(
+        Recipe(
+            name = "Delicious Meal",
+            description = "Meal Description",
+            thumbnail = "url.to.image"
+        )
+    )
+}
