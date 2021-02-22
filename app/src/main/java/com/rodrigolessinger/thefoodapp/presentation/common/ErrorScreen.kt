cd6abf32ef584a3566bf9e85e48b46ae3d199555
Rodrigo Lessinger <rodrigo.lessinger@nubank.com.br>
2021-02-21 23:01:10 -0300

    Improve error screen layout

--- app/src/main/java/com/rodrigolessinger/thefoodapp/presentation/common/ErrorScreen.kt
+++ app/src/main/java/com/rodrigolessinger/thefoodapp/presentation/common/ErrorScreen.kt
@@ -1,16 +1,17 @@
 package com.rodrigolessinger.thefoodapp.presentation.common
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rodrigolessinger.thefoodapp.R
 
 @Composable
 fun ErrorScreen(onRetry: () -> Unit) {
@@ -19,10 +20,27 @@ fun ErrorScreen(onRetry: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("An error has occured")
-        Button(onClick = onRetry) {
-            Text("Retry")
+        MaterialTheme {
+            val typography = MaterialTheme.typography
+
+            Text(
+                text = "Oops!",
+                style = typography.h4
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "There was an error loading this content",
+                style = typography.body1
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            IconButton(onClick = onRetry) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_retry),
+                    contentDescription = "Retry",
+                )
+            }
         }
+
     }
 }
 
