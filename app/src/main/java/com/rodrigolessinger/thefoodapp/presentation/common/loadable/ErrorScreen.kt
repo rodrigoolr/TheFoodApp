becb1fa6657b3434e27758e917955a6b836b8f30
Rodrigo Lessinger <rodrigo.lessinger@nubank.com.br>
2021-02-22 11:56:18 -0300

    Use compose icon

--- app/src/main/java/com/rodrigolessinger/thefoodapp/presentation/common/loadable/ErrorScreen.kt
+++ app/src/main/java/com/rodrigolessinger/thefoodapp/presentation/common/loadable/ErrorScreen.kt
@@ -5,10 +5,11 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,8 +41,9 @@ fun ErrorScreen(onRetry: () -> Unit) {
 
             IconButton(onClick = onRetry) {
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_retry),
+                    Icons.Filled.Refresh,
                     contentDescription = stringResource(R.string.error_screen_retry_content_description),
+                    modifier = Modifier.size(48.dp)
                 )
             }
         }
